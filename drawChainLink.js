39b5848e6027e52e1b076cdf8f06dff68a5ad2c8
James Helton <trealtrix@gmail.com>
2018-03-03 18:10:32 -0500

    Add second chain link

--- drawChainLink.js
+++ drawChainLink.js
@@ -1,83 +1,129 @@
 function drawChainLink(ctx, xCen=0, yCen=0, xScale = 50, yScale = 50) {
 	let x, y;
-	let xc1, xc2, yc1, yc2;
+	let xc1, xc2, yc1, yc2, xDir, yDir;
+
+	function drawQuarter(xDir, yDir) {
+		ctx.beginPath();
+		x = xCen - xDir * xScale/2;
+		y = yCen - (1 * yDir) + yDir * yScale/2;
+		ctx.moveTo(x, y);
+
+		y -= 2 * yDir * yScale;
+		ctx.lineTo(x, y);
+
+		xc1 = x;
+		yc1 = y - yDir * yScale*2/3;
+		yc2 = y - yDir * yScale*2/3;
+		x += xDir * xScale;
+		xc2 = x;
+		ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+
+		x = x + xDir * xScale;
+		ctx.lineTo(x, y);
+
+		xc1 = x;
+		yc1 = y - yDir * yScale * 2;
+		yc2 = yc1;
+		x = x - 3 * xDir *  xScale;
+		xc2 = x;
+		ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+
+		y = y + 2 * yDir * yScale;
+		ctx.lineTo(x, y);
+		ctx.closePath();
+
+		ctx.fill();
+		ctx.stroke();
+	}
+
+	function drawQuarter2(xDir, yDir) {
+		ctx.beginPath();
+		y = xCen - xDir * xScale/2;
+		x = yCen + yDir * yScale/2;
+		ctx.moveTo(y, x);
+
+		y -= 2 * yDir * yScale;
+		ctx.lineTo(y, x);
+
+		xc1 = x;
+		yc1 = y - yDir * yScale*2/3;
+		yc2 = y - yDir * yScale*2/3;
+		x += xDir * xScale;
+		xc2 = x;
+		ctx.bezierCurveTo(yc1, xc1, yc2, xc2, y, x);
+
+		x = x + xDir * xScale;
+		ctx.lineTo(y, x);
+
+		xc1 = x;
+		yc1 = y - yDir * yScale * 2;
+		yc2 = yc1;
+		x = x - 3 * xDir *  xScale;
+		xc2 = x;
+		ctx.bezierCurveTo(yc1, xc1, yc2, xc2, y, x);
+
+		y = y + 2 * yDir * yScale;
+		ctx.lineTo(y, x);
+		ctx.closePath();
+
+		ctx.fill();
+		ctx.stroke();
+	}
+
 
 	ctx.save();
 
 	ctx.strokeStyle="white";
-	ctx.beginPath();
-	ctx.moveTo(xCen + xScale/2, yCen + yScale/2);
-	ctx.lineTo(xCen + xScale/2, yCen - yScale/2);
-	ctx.lineTo(xCen - xScale/2, yCen - yScale/2);
-	ctx.lineTo(xCen - xScale/2, yCen + yScale/2);
-	ctx.closePath();
-	ctx.stroke();
+	// ctx.beginPath();
+	// ctx.moveTo(xCen + xScale/2, yCen + yScale/2);
+	// ctx.lineTo(xCen + xScale/2, yCen - yScale/2);
+	// ctx.lineTo(xCen - xScale/2, yCen - yScale/2);
+	// ctx.lineTo(xCen - xScale/2, yCen + yScale/2);
+	// ctx.closePath();
+	// ctx.stroke();
 
+	// xDir = 1;
+	// yDir = 1;
+	// ctx.beginPath();
+	// x = xCen - xDir * xScale/2;
+	// y = yCen + yDir * yScale/2;
+	// ctx.moveTo(x, y);
 
-	ctx.beginPath();
-	x = xCen - xScale/2;
-	y = yCen + yScale/2;
-	ctx.moveTo(x, y);
+	// y -= 2 * xDir * yScale;
+	// ctx.lineTo(x, y);
 
-	y -= 2 * yScale;
-	ctx.lineTo(x, y);
+	// xc1 = x;
+	// yc1 = y - xDir * yScale*2/3;
+	// yc2 = y - yDir * yScale*2/3;
+	// x += xDir * xScale;
+	// xc2 = x;
+	// ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
 
-	xc1 = x;
-	yc1 = y - yScale*2/3;
-	yc2 = y - yScale*2/3;
-	x += xScale;
-	xc2 = x;
-	ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+	// x = x + xDir * xScale;
+	// ctx.lineTo(x, y);
 
-	x = x + xScale;
-	ctx.lineTo(x, y);
+	// xc1 = x;
+	// yc1 = y - yDir * yScale * 2;
+	// yc2 = yc1;
+	// x = x - 3 * xDir *  xScale;
+	// xc2 = x;
+	// ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
 
-	xc1 = x;
-	yc1 = y - yScale * 2;
-	yc2 = yc1;
-	x = x - 3 * xScale;
-	xc2 = x;
-	ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+	// y = y + 2 * yDir * yScale;
+	// ctx.lineTo(x, y);
+	// ctx.closePath();
 
-	y = y + 2 * yScale;
-	ctx.lineTo(x, y);
-	ctx.closePath();
+	// ctx.fill();
+	// ctx.stroke();
 
-	ctx.fill();
-	ctx.stroke();
-
-
-	ctx.beginPath();
-	x = xCen - xScale/2;
-	y = yCen + yScale/2;
-	ctx.moveTo(2 * xCen - x, 2 * yCen - y);
-
-	y -= 2 * yScale;
-	ctx.lineTo(2 * xCen - x, 2 * yCen - y);
-
-	xc1 = x;
-	yc1 = y - yScale*2/3;
-	yc2 = y - yScale*2/3;
-	x += xScale;
-	xc2 = x;
-	ctx.bezierCurveTo(2 * xCen - xc1, 2 * yCen - yc1, 2 * xCen - xc2, 2 * yCen - yc2, 2 * xCen - x, 2 * yCen - y);
-
-	x = x + xScale;
-	ctx.lineTo(2 * xCen - x, 2 * yCen - y);
-
-	xc1 = x;
-	yc1 = y - yScale * 2;
-	yc2 = yc1;
-	x = x - 3 * xScale;
-	xc2 = x;
-	ctx.bezierCurveTo(2 * xCen - xc1, 2 * yCen - yc1, 2 * xCen - xc2, 2 * yCen - yc2, 2 * xCen - x, 2 * yCen - y);
-
-	y = y + 2 * yScale;
-	ctx.lineTo(2 * xCen - x, 2 * yCen - y);
-	ctx.closePath();
-
-	ctx.fill();
-	ctx.stroke();
+	ctx.strokeStyle="grey";
+	ctx.fillStyle="grey";
+	drawQuarter2(1, -1);
+	drawQuarter2(-1, 1);
+	ctx.strokeStyle="white";
+	ctx.fillStyle="white";
+	drawQuarter(1, 1);
+	drawQuarter(-1, -1);
 
 
 
