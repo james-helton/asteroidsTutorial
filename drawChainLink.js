c978d47968c3ced830f3cce045ee01d188891b56
James Helton <trealtrix@gmail.com>
2018-03-03 18:17:48 -0500

    Refactor to use one chain function

--- drawChainLink.js
+++ drawChainLink.js
@@ -2,74 +2,65 @@ function drawChainLink(ctx, xCen=0, yCen=0, xScale = 50, yScale = 50) {
 	let x, y;
 	let xc1, xc2, yc1, yc2, xDir, yDir;
 
-	function drawQuarter(xDir, yDir) {
+	function drawQuarter(xDir, yDir, rotate) {
 		ctx.beginPath();
-		x = xCen - xDir * xScale/2;
-		y = yCen - (1 * yDir) + yDir * yScale/2;
-		ctx.moveTo(x, y);
+		if(rotate) {
+			y = xCen - xDir * xScale/2;
+			x = yCen + yDir * yScale/2;
+			ctx.moveTo(y, x);
+		} else {
+			x = xCen - xDir * xScale/2;
+			y = yCen - (1 * yDir) + yDir * yScale/2;
+			ctx.moveTo(x, y);
+		}
 
 		y -= 2 * yDir * yScale;
-		ctx.lineTo(x, y);
+		if(rotate) {
+			ctx.lineTo(y, x);
+		} else {
+			ctx.lineTo(x, y);
+		}
 
 		xc1 = x;
 		yc1 = y - yDir * yScale*2/3;
 		yc2 = y - yDir * yScale*2/3;
 		x += xDir * xScale;
 		xc2 = x;
-		ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+		if(rotate) {
+			ctx.bezierCurveTo(yc1, xc1, yc2, xc2, y, x);
+		} else {
+			ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+		}
 
 		x = x + xDir * xScale;
-		ctx.lineTo(x, y);
+		if(rotate) {
+			ctx.lineTo(y, x);
+		} else {
+			ctx.lineTo(x, y);
+		}
 
 		xc1 = x;
 		yc1 = y - yDir * yScale * 2;
 		yc2 = yc1;
 		x = x - 3 * xDir *  xScale;
 		xc2 = x;
-		ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+		if(rotate) {
+			ctx.bezierCurveTo(yc1, xc1, yc2, xc2, y, x);
+		} else {
+			ctx.bezierCurveTo(xc1, yc1, xc2, yc2, x, y);
+		}
 
 		y = y + 2 * yDir * yScale;
-		ctx.lineTo(x, y);
+		if(rotate) {
+			ctx.lineTo(y, x);
+		} else {
+			ctx.lineTo(x, y);
+		}
 		ctx.closePath();
 
 		ctx.fill();
 		ctx.stroke();
 	}
-
-	function drawQuarter2(xDir, yDir) {
-		ctx.beginPath();
-		y = xCen - xDir * xScale/2;
-		x = yCen + yDir * yScale/2;
-		ctx.moveTo(y, x);
-
-		y -= 2 * yDir * yScale;
-		ctx.lineTo(y, x);
-
-		xc1 = x;
-		yc1 = y - yDir * yScale*2/3;
-		yc2 = y - yDir * yScale*2/3;
-		x += xDir * xScale;
-		xc2 = x;
-		ctx.bezierCurveTo(yc1, xc1, yc2, xc2, y, x);
-
-		x = x + xDir * xScale;
-		ctx.lineTo(y, x);
-
-		xc1 = x;
-		yc1 = y - yDir * yScale * 2;
-		yc2 = yc1;
-		x = x - 3 * xDir *  xScale;
-		xc2 = x;
-		ctx.bezierCurveTo(yc1, xc1, yc2, xc2, y, x);
-
-		y = y + 2 * yDir * yScale;
-		ctx.lineTo(y, x);
-		ctx.closePath();
-
-		ctx.fill();
-		ctx.stroke();
-	}
-
 
 	ctx.save();
 
@@ -118,8 +109,8 @@ function drawChainLink(ctx, xCen=0, yCen=0, xScale = 50, yScale = 50) {
 
 	ctx.strokeStyle="grey";
 	ctx.fillStyle="grey";
-	drawQuarter2(1, -1);
-	drawQuarter2(-1, 1);
+	drawQuarter(1, -1, true);
+	drawQuarter(-1, 1, true);
 	ctx.strokeStyle="white";
 	ctx.fillStyle="white";
 	drawQuarter(1, 1);
