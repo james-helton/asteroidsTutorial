3fcb4fac200ea1d7f6c2e8b3ee422fe58e55bc3e
James Helton <trealtrix@gmail.com>
2018-03-03 15:33:44 -0500

    Make drawGrid more robust

--- drawGrid.js
+++ drawGrid.js
@@ -1,26 +1,32 @@
-function drawGrid() {
-	var canvas = document.getElementById('asteroids');
-	var context = canvas.getContext('2d');
+function drawGrid(ctx, minor, major, stroke, fill) {
+	minor = minor || 10;
+	major = major || 50;
+	stroke = stroke || '#0F0';
+	fill = fill || '#090';
 
-	context.strokeStyle = '#0F0';
-	context.fillStyle = '#090';
+	ctx.save();
 
-	for (let x = 0; x < canvas.width; x+=10)
+	ctx.strokeStyle = stroke;
+	ctx.fillStyle = fill;
+
+	for (let x = 0; x < canvas.width; x+=minor)
 	{
-		context.beginPath();
-		context.moveTo(x,0);
-		context.lineTo(x, canvas.height);
-		context.lineWidth = x % 50 == 0 ? 0.5 : 0.25;
-		if(x%50 == 0) {context.fillText(x, x, 10);}
-		context.stroke();
+		ctx.beginPath();
+		ctx.moveTo(x,0);
+		ctx.lineTo(x, canvas.height);
+		ctx.lineWidth = x % major == 0 ? 0.5 : 0.25;
+		if(x%major == 0) {ctx.fillText(x, x, 10);}
+		ctx.stroke();
 	}
-	for (let y = 0; y < canvas.height; y+=10)
+	for (let y = 0; y < canvas.height; y+=minor)
 	{
-		context.beginPath();
-		context.moveTo(0, y);
-		context.lineTo(canvas.width, y);
-		context.lineWidth = y % 50 == 0 ? 0.5 : 0.25;
-		if(y%50 == 0) {context.fillText(y, 0, y + 10);}
-		context.stroke();
+		ctx.beginPath();
+		ctx.moveTo(0, y);
+		ctx.lineTo(canvas.width, y);
+		ctx.lineWidth = y % major == 0 ? 0.5 : 0.25;
+		if(y%major == 0) {ctx.fillText(y, 0, y + 10);}
+		ctx.stroke();
 	}
+
+	ctx.restore();
 }
