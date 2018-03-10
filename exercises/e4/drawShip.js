ca857cbee134f63be15d21fef832a492d4d89b45
James Helton <trealtrix@gmail.com>
2018-03-09 22:23:58 -0500

    Exercise 4: Add basic ship

---
+++ exercises/e4/drawShip.js
@@ -0,0 +1,36 @@
+function drawShip(ctx, x, y, r, options = {}) {
+	ctx.save();
+
+	// options = options || {};
+
+	if(options.guide) {
+		ctx.strokeStyle = 'white';
+		ctx.fillStyle = 'rgba(0,0,0,0.25)';
+		ctx.lineWidth = 0.5;
+		ctx.beginPath();
+		ctx.arc(x, y, r, 0, 2 * Math.PI);
+		ctx.stroke();
+		ctx.fill();
+	}
+
+	ctx.lineWidth = options.lineWidth || 2;
+	ctx.strokeStyle = options.strokeStyle || 'white';
+	ctx.fillStyle = options.fillStyle || 'black';
+	let angle = (options.angle || 0.5 * Math.PI) / 2;
+
+	ctx.beginPath();
+	ctx.moveTo(x + r, y);
+	ctx.lineTo(
+		x + Math.cos(Math.PI - angle) * r,
+		y + Math.sin(Math.PI - angle) * r
+	);
+	ctx.lineTo(
+		x + Math.cos(Math.PI + angle) * r,
+		y + Math.sin(Math.PI + angle) * r
+	);
+	ctx.closePath();
+	ctx.fill();
+	ctx.stroke();
+
+	ctx.restore();
+}
