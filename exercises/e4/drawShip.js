cbe6d255cc921922e3ca60068972c9c4eee4b945
James Helton <trealtrix@gmail.com>
2018-03-10 00:17:42 -0500

    Exercise 4: Add curve for ship sides

--- exercises/e4/drawShip.js
+++ exercises/e4/drawShip.js
@@ -5,7 +5,8 @@ function drawShip(ctx, options = {}) {
 	 *          x:	x location                      *
 	 *          y:	y location                      *
 	 *          r:	radius                          *
-	 *.         c:  curve                           *
+	 *         c1:  curve at rear of ship           *
+	 *         c2:  curve of sides of ship          *
 	 *         sa:	ship angle (direction)          *
 	 *         ba:	body angle (size)               *
 	 *      guide:	whether guide bubble displays   *
@@ -23,7 +24,8 @@ function drawShip(ctx, options = {}) {
 	let x = options.x || 0;
 	let y = options.y || 0;
 	let r = options.r || 50;
-	let c = options.c || 0.5;
+	let c1 = options.c1 || 0.25;
+	let c2 = options.c2 || 0.75;
 	ctx.translate(x, y)
 	ctx.rotate(rotation);
 
@@ -43,28 +45,69 @@ function drawShip(ctx, options = {}) {
 	let pointAngle = (options.ba || 0.5 * Math.PI) / 2;
 	ctx.beginPath();
 	ctx.moveTo(0 + r, 0);
-	ctx.lineTo(
+	ctx.quadraticCurveTo(
+		Math.cos(pointAngle) * r * c2,
+		Math.sin(pointAngle) * r * c2,
 		0 + Math.cos(Math.PI - pointAngle) * r,
 		0 + Math.sin(Math.PI - pointAngle) * r
 	);
 	ctx.quadraticCurveTo(
-		r * c - r, 0,
+		r * c1 - r,
+		0,
 		0 + Math.cos(Math.PI + pointAngle) * r,
 		0 + Math.sin(Math.PI + pointAngle) * r
 	);
-	ctx.closePath();
+	ctx.quadraticCurveTo(
+		Math.cos(-pointAngle) * r * c2,
+		Math.sin(-pointAngle) * r * c2,
+		r,
+		0
+	);
 	ctx.fill();
 	ctx.stroke();
 
 	if(options.guide) {
 		ctx.strokeStyle = 'white';
 		ctx.lineWidth = 0.5;
+		// back of ship
 		ctx.beginPath();
 		ctx.moveTo(-r, 0);
 		ctx.lineTo(0, 0);
 		ctx.stroke();
 		ctx.beginPath();
-		ctx.arc(r * c - r, 0, r/50, 0, 2 * Math.PI);
+		ctx.arc(r * c1 - r, 0, r/50, 0, 2 * Math.PI);
+		ctx.stroke();
+
+		// side 1 of ship
+		ctx.beginPath();
+		ctx.moveTo(
+			Math.cos(pointAngle) * r,
+			Math.sin(pointAngle) * r
+		);
+		ctx.lineTo(0, 0);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(
+			Math.cos(pointAngle) * r * c2,
+			Math.sin(pointAngle) * r * c2,
+			r/40, 0, 2 * Math.PI
+		);
+		ctx.stroke();
+
+		// side 2 of ship
+		ctx.beginPath();
+		ctx.moveTo(
+			Math.cos(-pointAngle) * r,
+			Math.sin(-pointAngle) * r
+		);
+		ctx.lineTo(0, 0);
+		ctx.stroke();
+		ctx.beginPath();
+		ctx.arc(
+			Math.cos(-pointAngle) * r * c2,
+			Math.sin(-pointAngle) * r * c2,
+			r/40, 0, 2 * Math.PI
+		);
 		ctx.stroke();
 	}
 
